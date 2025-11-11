b440dc643d52fb1f277b3cbc9559fc9a94782875
chgwyellow <dinnis1107@gmail.com>
2025-11-12 00:01:08 +0800

    Establish the RF model and evaluate it.

--- src/modeling/evaluate_model.py
+++ src/modeling/evaluate_model.py
@@ -2,7 +2,7 @@ import datetime
 import json
 
 import joblib
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.metrics import mean_absolute_error, r2_score, root_mean_square_error
 
 from src.config import MODEL_DIR
 from src.utils.emoji_log import info, save
@@ -10,28 +10,27 @@ from src.utils.emoji_log import info, save
 
 def evaluate_and_save(model, X_test, y_test, model_name: str):
     """Evaluate model and save results."""
+    # === 1. Predict ===
     y_pred = model.predict(X_test)
 
+    # === 2. Compute metrics ===
     mae = mean_absolute_error(y_test, y_pred)
-    rmse = mean_squared_error(y_test, y_pred)
+    rmse = root_mean_square_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
 
-    metrics = {"MAE": mae, "rmse": rmse, "R2": r2}
-
+    metrics = {"MAE": mae, "RMSE": rmse, "R2": r2}
     info(f"{model_name} Results: {metrics}")
 
-    # Save model
-    timestamp = datetime.datatime.now().strftime("%Y%m%d_%H%M")
+    # === 3. Save model ===
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M")
     model_path = MODEL_DIR / f"{model_name}_{timestamp}.pkl"
     joblib.dump(model, model_path)
-    save(f"{model} saved at {model_path}")
+    save(f"{model_name} model saved at {model_path}")
 
-    # Save metrics
-    timestamp = datetime.datatime.now().strftime("%Y%m%d_%H%M")
+    # === 4. Save metrics ===
     metrics_path = MODEL_DIR / f"{model_name}_metrics_{timestamp}.json"
     with open(metrics_path, "w", encoding="utf-8") as f:
         json.dump(metrics, f, indent=4)
-
     save(f"Metrics saved at {metrics_path}")
 
     return metrics
